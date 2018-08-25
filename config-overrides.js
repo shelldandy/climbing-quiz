7a14a89f3771ba4546210494f2bbac0164852b92
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-08-25 11:37:24 -0500

    setting op config-overrides with aliases

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,16 @@
-const rewireStyledComponents = require('react-app-rewire-styled-components');
+const rewireStyledComponents = require('react-app-rewire-styled-components')
 const rewireReactHotLoader = require('react-app-rewire-hot-loader')
+const rewireAliases = require('react-app-rewire-aliases')
+const { paths } = require('react-app-rewired')
+const path = require('path')
 
 module.exports = function override(config, env) {
-  config = rewireStyledComponents(config, env);
+  config = rewireStyledComponents(config, env)
   config = rewireReactHotLoader(config, env)
+  config = rewireAliases.aliasesOptions({
+    '@components': path.resolve(__dirname, `${paths.appSrc}/components/`),
+    '@container': path.resolve(__dirname, `${paths.appSrc}/containers/`),
+  })(config, env)
 
-  // TODO: Add useful aliases
-  // https://github.com/aze3ma/react-app-rewire-aliases
-
-  return config;
+  return config
 }
