0371aa76710bff7018637144b72fe70bf549d3fd
Miguel Palau Zarza <mpalauzarza@gmail.com>
2022-05-30 14:48:18 -0500

    render quizData

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import Proptypes from 'prop-types'
+import quizData from './quizData'
 
-const Quiz = () => {
+const Quiz = ({ quizData }) => {
   return (
-    <div>Quiz</div>
+    <section>
+      {quizData && quizData.map(({ label, questions }, idx) => (
+        <div key={`${label}_${idx}`}>
+          <h2>{label}</h2>
+          <form>
+            {questions.map(({ question, options }, qdx) => {
+              const name = `${idx}__${qdx}`;
+              return (
+              <div key={`${question}_${qdx}`}>
+                <h3>{question}</h3>
+                {options.map((option, odx) => {
+                const optionName = `${idx}__${qdx}--${odx}`;
+                  return (
+                    <div key={`${option}_${odx}`}>
+                      <label htmlFor={optionName}>{option}</label>
+                      <input type="radio" value={odx+1} name={name} id={optionName} />
+                    </div>
+                )
+                })}
+              </div>
+            )
+            })}
+          </form>
+        </div>
+      ))}
+    </section>
   );
 };
 
-Quiz.propTypes = {};
+Quiz.propTypes = {
+  quizData: Proptypes.array,
+};
 
-Quiz.defaultProps = {};
+Quiz.defaultProps = {
+  quizData
+};
 
 export default Quiz;
---
+++ src/components/Quiz/quizData.js
@@ -0,0 +1,83 @@
+const climbingExperience = {
+  label: "Climbing Experience",
+  questions: [
+    {
+      question: "How long have you been climbing?",
+      options: [
+        "less than six months",
+        "six to twelve months",
+        "one to four years",
+        "five to ten years",
+        "more than ten years",
+      ]
+    },
+    {
+      question: "On average how may days per month do you climb (both indoors and outdoors)",
+      options: [
+        "one day or less",
+        "two or three days",
+        "four to eight days",
+        "nine to twelve days",
+        "more than twelve days",
+      ]
+    },
+    {
+      question: "How many different climbing areas and gyms have you visited in the last year?",
+      options: [
+        "1",
+        "2-4",
+        "5-9",
+        "10-15",
+        "16+",
+      ]
+    },
+    {
+      question: "How many of the following styles of climbing have you been active in over the last year: bouldering, roped gym climbing, outdoor sport climbing, follow trad climbs, leading trad climbs, big wall",
+      options: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5 or 6",
+      ]
+    },
+    {
+      question: "How many of the following types of climbing have you engaged in onver the last three months: slab climbing, face climbing, crack climbing, overhanging face climbing, pocket climbing, roofs?",
+      options: [
+        "1 or 2",
+        "3",
+        "4",
+        "5",
+        "6",
+      ]
+    },
+  ],
+  analysis: [
+    {
+      level: 5,
+      label: "As a novice climber you possess tremendous potential to improve. A regular schedule of climbing and a willingness to stretch your boundaries will yield rapid gains in ability."
+    },
+    {
+      level: 10,
+      label: "Your experience is limited or narrow in scope, but huge gains await you given a commitment to climb more often and explore new types of climbing."
+    },
+    {
+      level: 15,
+      label: "Your experience is average."
+    },
+    {
+      level: 20,
+      label: "Your experience is above average."
+    },
+    {
+      level: 23,
+      label: "You are well on your way to mastery!"
+    },
+  ]
+}
+
+const quizData = [
+  climbingExperience,
+]
+
+export default quizData
