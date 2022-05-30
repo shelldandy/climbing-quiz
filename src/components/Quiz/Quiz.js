e2582a917e4842a6f7e92bc033fd1cc1b929c99c
Miguel Palau Zarza <mpalauzarza@gmail.com>
2022-05-30 14:06:38 -0500

    initial HTML

---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Quiz = () => {
+  return (
+    <div>Quiz</div>
+  );
+};
+
+Quiz.propTypes = {};
+
+Quiz.defaultProps = {};
+
+export default Quiz;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import * as React from "react"
+import { Quiz } from '../components';
 
 const IndexPage = () => {
   return (
     <main>
       <title>Climbing Quiz</title>
       <h1>Climbing Quiz</h1>
+      <p>Taken from Eric Hörst's book: <a title="Get the book here" href="https://trainingforclimbing.com/buy-books/the-rock-climbers-exercise-guide/">The Rock Climber's Exercise Guide</a></p>
+      <p>Ported to React.js by <a href="https://www.instagram.com/bowlinedandy/" title="@bowlinedandy on Instagram">@BowlineDandy</a></p>
+      <Quiz />
     </main>
   )
 }
